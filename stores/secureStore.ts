5fe1888620ac96321d7fcfa40ee2220c2aa856ca
dd060606 <dd06.developpement@gmail.com>
2025-05-15 15:26:24 +0200

    feat(stores): add desktop support for secure and normal stores

--- stores/secureStore.ts
+++ stores/secureStore.ts
@@ -1,9 +1,15 @@
+import { sendTauriCommand } from "@/utils/desktop";
 import { getItemAsync, setItemAsync, deleteItemAsync } from "expo-secure-store";
+import { Platform } from "react-native";
 
 //Permet de stocker des données sensibles de manière sécurisée (accessible après redémarrage de l'application = fichier de stockage)
 //Toutes les données stockées par le secure store sont chiffrés par Expo et sont uniquement accessible par l'application
 export async function getSecureStoreItem(key: string): Promise<string | null> {
     try {
+        if (Platform.OS === "web") {
+            //Si on est sur l'appli de bureau, on utilise le secure store de Tauri
+            return await sendTauriCommand("get_secure_item", { key });
+        }
         return await getItemAsync(key);
     } catch (e) {
         console.error("Failed to get item from SecureStore", e);
@@ -12,9 +18,13 @@ export async function getSecureStoreItem(key: string): Promise<string | null> {
 }
 export async function setSecureStoreItem(
     key: string,
-    value: string,
+    value: string
 ): Promise<void> {
     try {
+        if (Platform.OS === "web") {
+            //Si on est sur l'appli de bureau, on utilise le secure store de Tauri
+            return await sendTauriCommand("set_secure_item", { key, value });
+        }
         await setItemAsync(key, value);
     } catch (e) {
         console.error("Failed to set item in SecureStore", e);
@@ -23,6 +33,10 @@ export async function setSecureStoreItem(
 
 export async function removeSecureStoreItem(key: string): Promise<void> {
     try {
+        if (Platform.OS === "web") {
+            //Si on est sur l'appli de bureau, on utilise le secure store de Tauri
+            return await sendTauriCommand("delete_secure_item", { key });
+        }
         await deleteItemAsync(key);
     } catch (e) {
         console.error("Failed to remove item from SecureStore", e);
--- stores/storage.ts
+++ stores/storage.ts
@@ -1,9 +1,19 @@
+import { sendTauriCommand } from "@/utils/desktop";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
 // On sauvegarde une clé dans le stockage
 export async function saveStateToStorage(key: string, value: any) {
     try {
         const jsonValue = JSON.stringify(value);
+        if (Platform.OS === "web") {
+            //Si on est sur l'appli de bureau, on utilise le secure store de Tauri
+            await sendTauriCommand("set_item", {
+                key,
+                value: jsonValue
+            });
+            return;
+        }
         await AsyncStorage.setItem(key, jsonValue);
     } catch (e) {
         console.error("Failed to save the state to AsyncStorage", e);
@@ -12,6 +22,11 @@ export async function saveStateToStorage(key: string, value: any) {
 // On récupère une clé depuis le stockage
 export async function loadStateFromStorage(key: string) {
     try {
+        if (Platform.OS === "web") {
+            //Si on est sur l'appli de bureau, on utilise le secure store de Tauri
+            let jsonValue = await sendTauriCommand("get_item", { key });
+            return jsonValue != undefined ? JSON.parse(jsonValue) : null;
+        }
         const jsonValue = await AsyncStorage.getItem(key);
         return jsonValue != null ? JSON.parse(jsonValue) : null;
     } catch (e) {
@@ -23,6 +38,11 @@ export async function loadStateFromStorage(key: string) {
 // On supprime une clé depuis le stockage
 export async function clearStateFromStorage(key: string) {
     try {
+        if (Platform.OS === "web") {
+            //Si on est sur l'appli de bureau, on utilise le secure store de Tauri
+            await sendTauriCommand("delete_item", { key });
+            return;
+        }
         await AsyncStorage.removeItem(key);
     } catch (e) {
         console.error("Failed to clear the state from AsyncStorage", e);
